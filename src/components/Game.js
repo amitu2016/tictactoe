bc392041df61004f2aa1a017dad53ce26a98fc35
Amit Upadhyay <amitu@cdac.in>
2022-12-24 01:30:11 +0530

    final commit

--- src/components/Game.js
+++ src/components/Game.js
@@ -19,6 +19,12 @@ export default class Game extends React.Component {
         const current = history[history.length - 1]
         const squares = current.squares
 
+        const winner = calculateWinner(squares)
+
+        if (winner || squares[i]) {
+            return
+        }
+
         squares[i] = this.state.xIsNext ? 'X' : 'O'
 
         this.setState({
@@ -34,11 +40,26 @@ export default class Game extends React.Component {
     render(){
         const history = this.state.history
         const current = history[this.state.stepNumber]
+
+        let status
+        const winner = calculateWinner(current.squares)
+
+        if (winner) {
+            status = 'Winner is ' + winner 
+        }else{
+            status = 'Next player is ' + (this.state.xIsNext ? 'X' : 'O')
+        }
+
+
+
         return (
 
             <div className="game">
                 <div className="game-board">
                     <Board clickAction = {(i) => this.handleClick(i)} squares = {current.squares}/>
+                </div>
+                <div className="game-info">
+                    <div className="status">{status}</div>
                 </div>
             </div>
     
@@ -47,3 +68,25 @@ export default class Game extends React.Component {
 
     
 }
+
+function calculateWinner (squares) {
+    const possibilities = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+  
+    for(let i = 0; i < possibilities.length; i++) {
+      const [a, b, c] = possibilities[i]
+      if(squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+        return squares[a]
+      }
+    }
+  
+    return null
+  }
